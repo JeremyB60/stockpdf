bb94006bf28fc2d7af64d693e3447130cb4e7326
JeremyB60 <ballin.jeremy@gmail.com>
2023-08-07 14:06:09 +0200

    Design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// JavaScript
 $(document).ready(function () {
   // Cacher la div "hide" au chargement de la page
   $(".dropzoneContainer .hide").hide();
@@ -6,5 +7,17 @@ $(document).ready(function () {
   $(".dropzoneContainer h2").click(function () {
     // Trouver la div "hide" spécifique à ce dropzoneContainer et faire slideToggle
     $(this).siblings(".hide").slideToggle();
+
+    // Récupérer le span contenu dans le h2
+    var arrowSpan = $(this).find("span.arrow");
+
+    // Vérifier s'il possède la classe "rotated"
+    if (arrowSpan.hasClass("rotated")) {
+      // Si oui, retirer la classe pour revenir à l'état initial
+      arrowSpan.removeClass("rotated");
+    } else {
+      // Sinon, ajouter la classe pour pivoter le symbole de 90 degrés
+      arrowSpan.addClass("rotated");
+    }
   });
 });
